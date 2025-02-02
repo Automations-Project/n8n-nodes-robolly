58bb4d1e313e975085303431b5727d776e9aeb04
Karek <142004971+Karek2001@users.noreply.github.com>
2025-02-02 20:17:48 +0300

    Update Robolly.node.ts

--- nodes/Robolly/Robolly.node.ts
+++ nodes/Robolly/Robolly.node.ts
@@ -36,7 +36,7 @@ export class Robolly implements INodeType {
 		],
 		properties: [
 			{
-				displayName: '<div hidden>🌟 Connect with the community! Join our official <a href="https://www.facebook.com/groups/robolly" target="_blank">Facebook Group</a> or engage in discussions on our unofficial <a href="https://discord.gg/YcV2hT87" target="_blank">Discord Server</a>. 🚀</div>',
+				displayName: '<div hidden>🌟 Connect with the community! Join our official <a href="https://www.facebook.com/groups/robolly" target="_blank">Facebook Group</a> or engage in discussions on our unofficial <a href="https://nskha.com/discord" target="_blank">Discord Server</a>. 🚀</div>',
 				name: 'tip',
 				type: 'notice',
 				default: '',
